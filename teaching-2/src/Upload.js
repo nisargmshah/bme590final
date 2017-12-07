89c71aa44857c15f5f2ee25357f861a8b892f4f3
Garren Angacian <GarrenA@Garrens-MacBook-Pro.local>
2017-12-07 15:04:31 -0500

    Began adding POST capabilities to get data to web server, waiting on server to be created but may make a fake one to test on in the meantime

--- teaching-2/src/Upload.js
+++ teaching-2/src/Upload.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DropzoneComponent from 'react-dropzone-component';
 import { UploadField } from '@navjobs/upload';
 import { AppRegistry, Text } from 'react';
+import axios from 'axios';
 
 var componentConfig = {
     iconFiletypes: ['.jpg', '.png', '.gif'],
@@ -14,21 +15,31 @@ class Upload extends Component {
 		super(props);
 		this.state = {
 			fileData: "",
-			percentage: NaN,
+			percentage: 90,
 			prediction: "",
-			numImages: NaN
+			numImages: 10000,
 		};
+	}
+
+	//Reason for .then; JS is asynchronous, so we're forcing it
+	// to wait for us to do the function when post is complete
+	// getData will take in fileData and 
+
+	postRawData = () => {
+		axios.post('http:', {fileData: this.state.fileData}).then( (response) => {
+			console.log('Uploaded Successfully')
+		});
 	}
 
 	onUpload = (files) => {
 		const reader = new FileReader();
 		const file = files[0];
 		reader.readAsDataURL(file);
-		//this.state.prediction  = "dying"
+		this.state.prediction  = "benign"
 		reader.onloadend = () => {
-			this.state.fileData = reader.result;
-			this.forceUpdate(); //forced rerender
+			this.setState({fileData: reader.result});
 			console.log(reader.result);
+			
 		}
 	}
 
@@ -39,12 +50,13 @@ class Upload extends Component {
 				<UploadField onFiles ={this.onUpload}>
 					<div>
 						Upload
-						{this.state.fileData}
+						<img src = {this.state.fileData} />
 						{this.state.prediction.length > 0 &&
         					<h5> Model Prediction: {this.state.prediction} </h5>
 						}
-						
-						<div style= { {marginTop:"300px"}} >
+						{this.postRawData({fileData:this.state.fileData})}
+
+						<div style= { {marginTop:"200px"}} >
 							<h5>Model Accuracy: {this.state.percentage} % </h5>
 							<h5>Number of Images Classified: {this.state.numImages}</h5>
 						</div>
@@ -54,7 +66,7 @@ class Upload extends Component {
 		)
 	}
 }
-//<img src = {this.state.fileData} />
+//{this.state.fileData}
 
 
 export default Upload;
