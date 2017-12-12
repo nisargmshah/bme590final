a5163ece5ce60eacbd55f7b5f545c27202c93fc1
VCM admin account <vcm@vcm-1839.vm.duke.edu>
2017-12-12 17:44:51 -0500

    successfully tested web service deployed publicly with front-end integration [closes #8, closes #7]

--- lesion_predictor.py
+++ lesion_predictor.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 #from get_prediction import get_prediction
 from ImageConversion import Image
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/upload_image", methods=['POST'])
 def lesion():
@@ -11,9 +12,8 @@ def lesion():
 
     :return: the predicted classification of the lesion (benign or malignant)
     """
-
-    image = request.args.get('fileData')
+    image = request.json['fileData']
     predictions = image # remove
     #lesion_image = Image(input_image=image)
     #(labels, predictions) = get_prediction(lesion_image.bin_from_64())
-    return predictions
+    return jsonify(predictions)
