377a20610f64bc487d7e8d19ea858ca4beee4aac
ras93 <ras93@duke.edu>
2017-12-12 15:45:08 -0500

    preliminary image conversion class: contains methods for returning in 2 and 64 form

--- ImageConversion.py
+++ ImageConversion.py
@@ -1,5 +1,23 @@
 import base64
 
 
-def convert_image_to_64(image):
-    return base64.b64encode(base64.b64decode(image))
+class Image:
+    """
+
+    """
+
+    def __init__(self, input_image):
+        """
+
+        :param image:
+        """
+        # test if in 64; if not, convert to 64
+        self.__image = input_image
+
+    # this function should just return image in 64
+    def bin_to_64(self):
+        return base64.b64encode(base64.b64decode(self.__image))
+
+
+    def bin_from_64(self):
+        return base64.b64decode(self.__image)
