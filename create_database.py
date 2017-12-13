b7fc1680bc77f13ddd00a1e1057d5d9604cf5f76
Nisarg Shah <nisarg1shah@gmail.com>
2017-12-13 15:44:51 -0500

    git commit -m "Changed connection string to work with vm"

--- create_database.py
+++ create_database.py
@@ -5,10 +5,11 @@ con = None
 dbname = "nms"
 
 try:
-	con = psycopg2.connect("dbname='nms' user='nms' host='localhost' password='bme590'")
+    #con = psycopg2.connect("dbname='bme590' user='postgres' host='db' port=5432 password='bme590'")
+	con = psycopg2.connect("dbname='bme590' user='postgres' host='vcm-1842.vm.duke.edu' port=5433 password='bme590'")
 	con.autocommit = True
 	cur = con.cursor()
-	cur.execute("CREATE TABLE melanoma_images(Image_Id INTEGER PRIMARY KEY, Model_Prediction VARCHAR(10), Actual_Result VARCHAR(10));")
+	cur.execute("CREATE TABLE melanoma_images(Image_Id VARCHAR(20) PRIMARY KEY, Model_Prediction VARCHAR(20), Actual_Result VARCHAR(20));")
 	cur.execute("CREATE TABLE web_client_users(User_Id INTEGER PRIMARY KEY, Image_Id INTEGER, Date_Uploaded TIMESTAMP);")
 	con.commit()
 except psycopg2.DatabaseError as e:
--- sql_data_gateway.py
+++ sql_data_gateway.py
@@ -1,12 +1,13 @@
 import psycopg2
 
 try:
-	con = psycopg2.connect("dbname='nms' user='nms' host='localhost' password = 'bme590'")
+	con = psycopg2.connect("dbname='bme590' user='postgres' host='vcm-1842.vm.duke.edu' port=5433 password = 'bme590'")
 except:
 	print("Unable to connect to the database")
 
 cur = con.cursor()
-cur.execute("""SELECT image_id from melanoma_images""")
+cur.execute("""Insert into melanoma_images (image_id, model_prediction, actual_result) values("ISIC_0000000", null, "benign");""")
+cur.execute("""SELECT image_id from melanoma_images;""")
 
 rows = cur.fetchall()
 
