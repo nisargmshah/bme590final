60c089110545c4ba998231d2281d9fdb8a829024
VCM admin account <vcm@vcm-1844.vm.duke.edu>
2017-12-13 15:42:01 -0500

    Fixing Connection Errors

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ RUN pip install Flask
 RUN pip install psycopg2
 RUN pip install pytest
 RUN pip install pytest-pep8
-run pip install pytest-cov
+RUN pip install pytest-cov
 RUN pip install numpy
 RUN pip install pandas
 RUN pip install flask
--- lesion_predictor.py
+++ lesion_predictor.py
@@ -4,7 +4,7 @@ from get_prediction import get_prediction
 from ImageConversion import Image
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 @app.route("/upload_image", methods=['POST'])
 def lesion():
@@ -13,8 +13,8 @@ def lesion():
     :return: the predicted classification of the lesion (benign or malignant)
     """
     image = request.json['fileData']
-    # the below line is for testing outside the docker container only
-    #predictions = image
+    the below line is for testing outside the docker container only
+    predictions = image
     lesion_image = Image(input_image=image)
     (labels, predictions) = get_prediction(lesion_image.bin_from_64())
     return jsonify(predictions)
